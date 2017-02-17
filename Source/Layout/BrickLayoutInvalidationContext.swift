8f0ed3ee52fcf5b6c7350ef1ba9f035921c5549b
agallo <agallo@wayfair.com>
2017-02-17 16:42:28 -0500

    fixed adding horizontal offset when we didnt need to

--- Source/Layout/BrickLayoutInvalidationContext.swift
+++ Source/Layout/BrickLayoutInvalidationContext.swift
@@ -136,7 +136,7 @@ class BrickLayoutInvalidationContext: UICollectionViewLayoutInvalidationContext 
             // Content Size
             let difference = CGSize(width: provider.contentSize.width - oldContentSize.width, height: provider.contentSize.height - oldContentSize.height)
             context.contentSizeAdjustment = difference
-            context.contentOffsetAdjustment = CGPoint(x: difference.width, y: -difference.height)
+            context.contentOffsetAdjustment = CGPoint(x: 0, y: -difference.height)
             context.invalidateItemsAtIndexPaths(updatedAttributes.map{ $0.indexPath })
         }
 
