9037187ad78b0430898e1257ee7a6d4723c75483
Justin Anderson <jus0610tin@gmail.com>
2017-03-14 15:14:24 -0400

    Fix severe memory leak when using async resizable cells

--- Tests/ViewControllers/BrickCollectionViewTests.swift
+++ Tests/ViewControllers/BrickCollectionViewTests.swift
@@ -16,7 +16,9 @@ class BrickCollectionViewTests: XCTestCase {
     override func setUp() {
         super.setUp()
 
-        brickView = BrickCollectionView(frame: CGRect(x: 0, y: 0, width: 320, height: 480))
+        autoreleasepool {
+            self.brickView = BrickCollectionView(frame: CGRect(x: 0, y: 0, width: 320, height: 480))
+        }
     }
 
     func testSetWrongCollectionViewLayout() {
@@ -682,6 +684,17 @@ class BrickCollectionViewTests: XCTestCase {
         XCTAssertEqual(section.brickCollectionView, brickView)
         XCTAssertEqual(innerSection.brickCollectionView, brickView)
     }
+    
+    func testThatBrickCollectionViewDoesNotCreateARetainCycleWithAsyncrhonousResizableCells() {
 
+        expectation(forNotification: "DeinitNotifyingAsyncBrickCell.deinit", object: nil, handler: nil)
+        autoreleasepool {
+            let brick = DeinitNotifyingAsyncBrick(size: BrickSize(width: .fill, height: .fill))
+            brickView.setupSingleBrickAndLayout(brick)
+            brickView = nil
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
 
 }
